1195d041cf9dbaa3f34615e817d6a5c726383489
octanym <cmrnmr@gmail.com>
2023-04-06 14:44:44 -0400

    refactored store using redux toolkit, replaced createStore with configureStore

--- lib/store/configure-store.dev.ts
+++ lib/store/configure-store.dev.ts
@@ -1,17 +1,22 @@
-import { configureStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import _thunk, { ThunkMiddleware } from "redux-thunk";
 import rootReducer from "../reducers/index";
 import effects from "../utils/effects";
 import * as plugins from "../utils/plugins";
 import writeMiddleware from "./write-middleware";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import { HyperState, HyperActions } from "../hyper";
 const thunk: ThunkMiddleware<HyperState, HyperActions> = _thunk;
 
 export default () => {
-  const enhancer = composeWithDevTools(
-    applyMiddleware(thunk, plugins.middleware, thunk, writeMiddleware, effects)
-  );
+  // const enhancer = composeWithDevTools(
+  //   applyMiddleware(thunk, plugins.middleware, thunk, writeMiddleware, effects)
+  // );
 
-  return configureStore({ reducer: rootReducer, enhancer });
+  //  configure store applies composeWithDevTools and applyMiddleware by default
+  // all that is needed is the array of middleware
+  return configureStore({
+    reducer: rootReducer,
+    middleware: [thunk, plugins.middleware, thunk, writeMiddleware, effects],
+  });
 };
